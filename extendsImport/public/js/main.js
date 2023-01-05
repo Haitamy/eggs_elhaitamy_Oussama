b8aca2b7495b779a9ecb5e49698ef65573477270
Haitamy <misterelhaitamy@gmail.com>
2023-01-05 17:57:46 +0100

    suite

--- extendsImport/public/js/main.js
+++ extendsImport/public/js/main.js
@@ -1,5 +1,8 @@
 import { Personne } from "./modules/objet.js";
 import { maison } from "./modules/objet.js";
+import { epicerie } from "./modules/objet.js";
 
 Personne.seDeplacer(maison.nom)
+console.log(Personne.nom + " est actuellement à la " + Personne.lieu);
+Personne.seDeplacer(epicerie.nom)
 console.log(Personne.nom + " est actuellement à la " + Personne.lieu);
