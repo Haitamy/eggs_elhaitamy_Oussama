d57e9f3642144fe45e01bf3f689fdcc1bd8da1a2
Haitamy <misterelhaitamy@gmail.com>
2023-01-05 16:31:52 +0100

    ok

---
+++ extendsImport/public/js/modules/objet.js
@@ -0,0 +1,14 @@
+export let Personne ={
+    nom : 'Oussama',
+    lieu :'maison',
+    argent : 50000,
+    mainDroite : [],
+    mainGauche : [],
+    seDeplacer(lieu){
+
+    }
+    
+
+}
+
+export let Lieu
